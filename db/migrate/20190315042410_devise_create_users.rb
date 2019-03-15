f8dabba2ae55726a86c4de58b6c18234e5f7b854
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-03-14 21:50:22 -0700

    added fullname, email, and admin to user columns

--- db/migrate/20190315042410_devise_create_users.rb
+++ db/migrate/20190315042410_devise_create_users.rb
@@ -34,6 +34,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
 
 
       t.timestamps null: false
+      t.string :fullname 
+      t.string :email
+      t.boolean :admin
+
     end
 
     add_index :users, :email,                unique: true
